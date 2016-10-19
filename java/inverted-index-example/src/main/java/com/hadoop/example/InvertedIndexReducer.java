7c3a45b82f48b076a31cb5510be802c1f54ca28b
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2016-10-20 01:57:31 +0300

    Fixed redicer

--- java/inverted-index-example/src/main/java/com/hadoop/example/InvertedIndexReducer.java
+++ java/inverted-index-example/src/main/java/com/hadoop/example/InvertedIndexReducer.java
@@ -8,16 +8,17 @@ import java.util.ArrayList;
 /**
  * @author Никита
  */
-public class InvertedIndexReducer extends Reducer<Text, Pair, Text, MyArrayWritable> {
+public class InvertedIndexReducer extends Reducer<Text, Pair, Text, Text> {
 	protected void reduce(Text key, Iterable<Pair> values, Context context)
 			throws java.io.IOException, InterruptedException {
 
         MyArrayWritable documentAndIndexList = new MyArrayWritable(Pair.class);
         ArrayList<Pair> arr = new ArrayList<Pair>();
+        String str = "";
         for (Pair docNameAndIndex : values) {
-            arr.add(docNameAndIndex);
+            str += docNameAndIndex.toString() + " ";
         }
         documentAndIndexList.set(arr.toArray(new Pair[arr.size()]));
-		context.write(key, documentAndIndexList);
+		context.write(key, new Text(str));
 	}
 }
